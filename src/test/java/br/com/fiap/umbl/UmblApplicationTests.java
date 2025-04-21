21f1b68800f02ffb0649f26a2e4dd529b6596493
samue <samuel.cruz_ntconsult@prestador.globo>
2025-04-21 20:59:57 -0300

    resolvendo problemas

--- src/test/java/br/com/fiap/umbl/UmblApplicationTests.java
+++ src/test/java/br/com/fiap/umbl/UmblApplicationTests.java
@@ -1,9 +1,11 @@
 package br.com.fiap.umbl;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Tag("unit")
 class UmblApplicationTests {
 
 	@Test
