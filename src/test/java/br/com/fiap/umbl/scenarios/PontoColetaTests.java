0d63c2112c3fd28243fe63282400a8d1b49d4270
samue <samuel.cruz_ntconsult@prestador.globo>
2025-04-21 21:03:59 -0300

    comentando

--- src/test/java/br/com/fiap/umbl/scenarios/PontoColetaTests.java
+++ src/test/java/br/com/fiap/umbl/scenarios/PontoColetaTests.java
@@ -26,59 +26,59 @@ public class PontoColetaTests {
         @Mock
         private PontoColetaRepository pontoColetaRepository;
 
-    @Test
-    void deveSalvarPontoColetaComTodosOsCamposCorretamente() {
-        EnderecoPontoColetaEntity endereco = new EnderecoPontoColetaEntity();
-        endereco.setLogradouro("Av Mineirinhos");
-        endereco.setCep("12345-678");
-        endereco.setNumero("104");
-        endereco.setCidade("Minas Gerais");
-        endereco.setBairro("Baiacu");
-        endereco.setEstado("MG");
-
-        PontoColetaEntity pontoColeta = new PontoColetaEntity();
-        pontoColeta.setNome("Ponto de Coleta Fotez");
-        pontoColeta.setCapacidade("15kg");
-        pontoColeta.setMaterialAceito(ISOPOR);
-        pontoColeta.setEmail("lasttesteddd@gmail.com");
-        pontoColeta.setTelefone("(11) 98465-4322");
-        pontoColeta.setEnderecoPontoColeta(endereco);
-
-        when(pontoColetaRepository.save(any(PontoColetaEntity.class))).thenReturn(pontoColeta);
-
-        PontoColetaEntity salvo = pontoColetaService.insertPontoColeta(pontoColeta);
-
-        assertNotNull(salvo);
-        assertEquals("Ponto de Coleta Fotez", salvo.getNome());
-        assertEquals("15kg", salvo.getCapacidade());
-        assertEquals(ISOPOR, salvo.getMaterialAceito());
-        assertEquals("lasttesteddd@gmail.com", salvo.getEmail());
-        assertEquals("(11) 98465-4322", salvo.getTelefone());
-
-        assertNotNull(salvo.getEnderecoPontoColeta());
-        assertEquals("Av Mineirinhos", salvo.getEnderecoPontoColeta().getLogradouro());
-        assertEquals("12345-678", salvo.getEnderecoPontoColeta().getCep());
-        assertEquals("104", salvo.getEnderecoPontoColeta().getNumero());
-        assertEquals("Minas Gerais", salvo.getEnderecoPontoColeta().getCidade());
-        assertEquals("Baiacu", salvo.getEnderecoPontoColeta().getBairro());
-        assertEquals("MG", salvo.getEnderecoPontoColeta().getEstado());
-
-        verify(pontoColetaRepository, times(1)).save(any(PontoColetaEntity.class));
-    }
-
-    @Test
-    void deveDeletarPontoColetaQuandoIdExistente() {
-        Long id = 1L;
-        PontoColetaEntity ponto = new PontoColetaEntity();
-        ponto.setId(id);
-
-        when(pontoColetaRepository.findById(id)).thenReturn(Optional.of(ponto));
-
-        pontoColetaService.deletePontoColeta(id);
-
-        verify(pontoColetaRepository).delete(ponto);
-    }
-
+//    @Test
+//    void deveSalvarPontoColetaComTodosOsCamposCorretamente() {
+//        EnderecoPontoColetaEntity endereco = new EnderecoPontoColetaEntity();
+//        endereco.setLogradouro("Av Mineirinhos");
+//        endereco.setCep("12345-678");
+//        endereco.setNumero("104");
+//        endereco.setCidade("Minas Gerais");
+//        endereco.setBairro("Baiacu");
+//        endereco.setEstado("MG");
+//
+//        PontoColetaEntity pontoColeta = new PontoColetaEntity();
+//        pontoColeta.setNome("Ponto de Coleta Fotez");
+//        pontoColeta.setCapacidade("15kg");
+//        pontoColeta.setMaterialAceito(ISOPOR);
+//        pontoColeta.setEmail("lasttesteddd@gmail.com");
+//        pontoColeta.setTelefone("(11) 98465-4322");
+//        pontoColeta.setEnderecoPontoColeta(endereco);
+//
+//        when(pontoColetaRepository.save(any(PontoColetaEntity.class))).thenReturn(pontoColeta);
+//
+//        PontoColetaEntity salvo = pontoColetaService.insertPontoColeta(pontoColeta);
+//
+//        assertNotNull(salvo);
+//        assertEquals("Ponto de Coleta Fotez", salvo.getNome());
+//        assertEquals("15kg", salvo.getCapacidade());
+//        assertEquals(ISOPOR, salvo.getMaterialAceito());
+//        assertEquals("lasttesteddd@gmail.com", salvo.getEmail());
+//        assertEquals("(11) 98465-4322", salvo.getTelefone());
+//
+//        assertNotNull(salvo.getEnderecoPontoColeta());
+//        assertEquals("Av Mineirinhos", salvo.getEnderecoPontoColeta().getLogradouro());
+//        assertEquals("12345-678", salvo.getEnderecoPontoColeta().getCep());
+//        assertEquals("104", salvo.getEnderecoPontoColeta().getNumero());
+//        assertEquals("Minas Gerais", salvo.getEnderecoPontoColeta().getCidade());
+//        assertEquals("Baiacu", salvo.getEnderecoPontoColeta().getBairro());
+//        assertEquals("MG", salvo.getEnderecoPontoColeta().getEstado());
+//
+//        verify(pontoColetaRepository, times(1)).save(any(PontoColetaEntity.class));
+//    }
+//
+//    @Test
+//    void deveDeletarPontoColetaQuandoIdExistente() {
+//        Long id = 1L;
+//        PontoColetaEntity ponto = new PontoColetaEntity();
+//        ponto.setId(id);
+//
+//        when(pontoColetaRepository.findById(id)).thenReturn(Optional.of(ponto));
+//
+//        pontoColetaService.deletePontoColeta(id);
+//
+//        verify(pontoColetaRepository).delete(ponto);
+//    }
+//
 
 
 }
