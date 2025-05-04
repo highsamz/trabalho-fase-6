e26de7b0b1cf4cc47dd20c5f6bb5938a91072b35
samue <samuel.cruz_ntconsult@prestador.globo>
2025-05-04 16:54:49 -0300

    Adicionando id no response

--- src/main/java/br/com/fiap/umbl/dto/listagem/ListagemPontoColeta.java
+++ src/main/java/br/com/fiap/umbl/dto/listagem/ListagemPontoColeta.java
@@ -5,12 +5,13 @@ import br.com.fiap.umbl.dto.cadastro.MaterialAceito;
 
 public record ListagemPontoColeta (
 
+        Long id,
         String nome,
         MaterialAceito materialAceito,
         String email,
         String telefone
 ){
     public ListagemPontoColeta(PontoColetaEntity pontoColeta){
-        this(pontoColeta.getNome(), pontoColeta.getMaterialAceito(), pontoColeta.getEmail(), pontoColeta.getTelefone());
+        this(pontoColeta.getId(), pontoColeta.getNome(), pontoColeta.getMaterialAceito(), pontoColeta.getEmail(), pontoColeta.getTelefone());
     }
 }
